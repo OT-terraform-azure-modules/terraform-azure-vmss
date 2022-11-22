e775ba5ebaa8b811cd5f4a4a8d200407a12acd4d
neelamkumariopstree <115863874+neelamkumariopstree@users.noreply.github.com>
2022-11-22 12:55:33 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "linux_virtual_machine_scale_set_id" {
   description = "The resource ID of the Linux Virtual Machine Scale Set."
-  value       = azurerm_linux_virtual_machine_scale_set.linux[count.index].id
+  value       = azurerm_linux_virtual_machine_scale_set.linux.*.id
 }
