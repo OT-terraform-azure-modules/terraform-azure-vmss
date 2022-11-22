2da91004134ffcfeef8297cb5a7abcfde14153bf
neelamkumariopstree <115863874+neelamkumariopstree@users.noreply.github.com>
2022-11-22 09:32:07 +0000

    Update output.tf

---
+++ output.tf
@@ -0,0 +1,4 @@
+output "linux_virtual_machine_scale_set_id" {
+  description = "The resource ID of the Linux Virtual Machine Scale Set."
+  value       = var.os_flavor == "Linux" ? element(concat(azurerm_linux_virtual_machine_scale_set.linux_vmss.*.id, [""]), 0) : null
+}
