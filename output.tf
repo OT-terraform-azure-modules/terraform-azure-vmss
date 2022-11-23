3262d33b3b50def220f1deb33098c7a1ec8c5a08
kulsinghdeep <93075914+kulsinghdeep@users.noreply.github.com>
2022-11-23 19:57:49 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "linux_virtual_machine_scale_set_id" {
   description = "The resource ID of the Linux Virtual Machine Scale Set."
   value       = azurerm_linux_virtual_machine_scale_set.linux[*].id
 }
+
+output "linux_virtual_machine_scale_set_ip" {
+  description = "The private ip of the Linux Virtual Machine Scale Set."
+  value       = azurerm_linux_virtual_machine_scale_set.linux[*].private_ip_address
+}
