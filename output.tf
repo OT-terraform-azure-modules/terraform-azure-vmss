23bfd5277729c6796750e49f6d2097a5febdfb80
kulsinghdeep <93075914+kulsinghdeep@users.noreply.github.com>
2022-11-23 20:40:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "linux_virtual_machine_scale_set_id" {
   value       = azurerm_linux_virtual_machine_scale_set.linux[*].id
 }
 
-output "linux_virtual_machine_scale_set_ip" {
-  description = "The private ip of the Linux Virtual Machine Scale Set."
-  value       = azurerm_linux_virtual_machine_scale_set.linux[*].private_ip_address
-}
+# output "linux_virtual_machine_scale_set_ip" {
+#   description = "The private ip of the Linux Virtual Machine Scale Set."
+#   value       = azurerm_linux_virtual_machine_scale_set.linux[*].private_ip_address
+# }
